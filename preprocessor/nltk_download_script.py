08c6e5c91a006cedd68c8330ec2b013f86fbe2e1
Marluk12321 <marko.lukic.12321@gmail.com>
2016-05-22 14:25:48 +0200

    skripta za skidanje svih nltk dodataka koje imam

---
+++ preprocessor/nltk_download_script.py
@@ -0,0 +1,8 @@
+import nltk
+
+
+nltk.download("stopwords")
+nltk.download("wordnet")
+nltk.download("averaged_perceptron_tagger")
+nltk.download("punkt")
+nltk.download("maxent_treebank_pos_tagger")
