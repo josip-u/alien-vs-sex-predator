f9bd57b7ddf52464f1e4f3a875e205a4302624a6
Marluk12321 <marko.lukic.12321@gmail.com>
2016-05-22 23:23:17 +0200

    \//\

---
+++ preprocessor/data_preparation.py
@@ -0,0 +1,102 @@
+import sys
+import os
+import copy
+import numpy as np
+
+import tfidf_builder
+sys.path.insert(0, os.path.abspath('../parser'))
+import message
+
+
+def get_filtered_out_words(word_frequency_dict, threshold):
+    filtered_out_words = set([])
+
+    for word in word_frequency_dict:
+        if word_frequency_dict[word] <= threshold:
+            filtered_out_words.add(word)
+
+    return filtered_out_words
+
+
+def to_document(messages):
+    document = ""
+    for message in messages:
+        document += " " + message._text
+    return document.strip()
+
+
+def to_documents(user_message_dict):
+    documents = []
+
+    for user in user_message_dict:
+        document = to_document(user_message_dict[user])
+        if len(document) > 0:
+            documents.append(document)
+
+    return documents
+
+
+def filter_by_message_count(user_message_dict, threshold):
+    filtered_user_message_dict = {}
+
+    for user in user_message_dict:
+        if len(user_message_dict[user]) > threshold:
+            filtered_user_message_dict[user] = user_message_dict[user]
+
+    return filtered_user_message_dict
+
+
+def filter_by_class_count(user_message_dict, user_class_dict, class_count_dict):
+    class_count_dict_copy = copy.deepcopy(class_count_dict)
+    filtered_user_message_dict = {}
+
+    for user in user_message_dict:
+        user_class = user_class_dict[user]
+        if class_count_dict_copy[user_class] > 0:
+            filtered_user_message_dict[user] = user_message_dict[user]
+            class_count_dict_copy[user_class] -= 1
+
+    return filtered_user_message_dict
+
+
+def to_time_vector(messages, time_splits):
+    assert len(time_splits) == 3, "expected 3 time splits"
+
+    time = 0
+    for message in messages:
+        time += message._time
+    time /= len(messages)
+
+    time_vector = [[0, 0, 0]]
+    if time >= time_splits[2] and time < time_splits[0]:
+        time_vector[0][0] = 1
+    elif time >= time_splits[0] and time < time_splits[1]:
+        time_vector[0][1] = 1
+    else:
+        time_vector[0][2] = 1
+    return np.array(time_vector)
+
+
+def to_input_vector(messages, tfidf_builder, time_splits):
+    document = to_document(messages)
+    tfidf_vector = tfidf_builder.to_tfidf_vector(document)
+    time_vector = to_time_vector(messages, time_splits)
+    return np.concatenate((tfidf_vector, time_vector), axis=1)
+
+
+def build_classifier_io(user_messages_dict, user_class_dict, tfidf_builder, fit_builder=False, time_splits=[7, 15, 23]):
+    if fit_builder:
+        documents = to_documents(user_messages_dict)
+        tfidf_builder.build_tfidf(documents)
+
+    user_vector = []
+    input_vector = []
+    output_vector = []
+
+    for user in user_messages_dict:
+        user_vector.append(user)
+        messages = user_messages_dict[user]
+        input_vector.append(to_input_vector(messages, tfidf_builder, time_splits)[0])
+        output_vector.append(user_class_dict[user])
+
+    return np.array(user_vector), np.array(input_vector), np.array(output_vector)
--- preprocessor/main.py
+++ preprocessor/main.py
@@ -1,5 +1,5 @@
-import os
-import sys
+#import os
+#import sys
 import pickle
 
 #import nltk
@@ -7,31 +7,14 @@ from nltk.stem.wordnet import WordNetLemmatizer
 #from sklearn.feature_extraction.text import TfidfVectorizer
 
 from tfidf_builder import TfidfBuilder
-from data_filtering import *
-
-
-'''
-def read_directory_files(path):
-    file_text_dict = {}
-
-    for directory_path, directories, files in os.walk(path):
-        for file in files:
-            if not file.endswith(".txt"):
-                continue
-
-            file_path = os.path.join(directory_path, file)
-            file = open(file_path, "r")
-            file_text_dict[file] = file.read()
-
-    return file_text_dict
-'''
+from data_preparation import *
 
 
 inputs = pickle.load(open("../inputs_dict_new.p", "rb"))
 outputs = pickle.load(open("../outputs_dict.p", "rb"))
 token_freq_dict = pickle.load(open("token_freq_dict.p", "rb"))
 
-
+'''
 user_count = len(inputs)
 for msg_treshold in range(1, 11):
     treshold_count = 0
@@ -72,19 +55,20 @@ for word in token_freq_dict:
         non_word_count += 1
 print(non_word_count, non_filtered_count, non_word_count * 100 / non_filtered_count)
 print()
-
+'''
 
 threshold = 5
 filtered_out_words = get_filtered_out_words(token_freq_dict, threshold)
 tfidf_builder = TfidfBuilder(filtered_out_words)
 
 ok_users = filter_by_message_count(inputs, threshold)
-documents = to_documents(ok_users)
+#documents = to_documents(ok_users)
 #tfidf_builder.to_tfidf(documents)
 #vec = tfidf_builder.to_tfidf_vector(documents[0])
 #print(type(vec), len(vec))
 
 class_count_dict = {}
+'''
 for user in ok_users:
     user_class = outputs[user]
     if user_class in class_count_dict:
@@ -93,16 +77,28 @@ for user in ok_users:
         class_count_dict[user_class] = 0
 print(class_count_dict)
 print()
+'''
 
 class_count_dict[0] = 120
 class_count_dict[1] = 120
-while class_count_dict[0] <= 20954:
+#max_negatives = 20000
+max_negatives = 120
+while True:
+    if class_count_dict[0] > max_negatives:
+        class_count_dict[0] = max_negatives
+
     ok_users2 = filter_by_class_count(ok_users, outputs, class_count_dict)
     documents = to_documents(ok_users2)
     tfidf_builder.to_tfidf(documents)
     vec = tfidf_builder.to_tfidf_vector(documents[0])
     print(class_count_dict[0], vec.shape)
-    class_count_dict[0] *= 2
+    users_vec, input_vec, output_vec = build_classifier_io(ok_users2, outputs, tfidf_builder)
+    print(users_vec.shape, input_vec.shape, output_vec.shape)
+
+    if class_count_dict[0] < max_negatives:
+        class_count_dict[0] *= 2
+    else:
+        break
 print()
 
 
